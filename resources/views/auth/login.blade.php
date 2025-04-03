7b54a311057f3a2d1fff073c06e42a04d4e29978
LoidMachado <loidmachado@outlook.com>
2025-04-03 20:53:26 +0100

    Update login page text and layout for improved user experience

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
     <!-- /.login-logo -->
     <div class="card card-outline card-primary">
         <div class="card-header text-center">
-            <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+            <a href="../../index2.html" class="h1"><b>Rascunhos</b></a>
         </div>
         <div class="card-body">
             <p class="login-box-msg">Sign in to start your session</p>
@@ -28,22 +28,12 @@
                 @csrf
                 <div class="input-group mb-3">
                     <input type="email" id="email" class="form-control" placeholder="Email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
                 </div>
                 <div class="input-group mb-3">
                     <input type="password" id="password" class="form-control" placeholder="Password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
                 </div>
                 <div class="row">
-                    <div class="col-8">
+                    <div class="col-6">
                         <div class="icheck-primary">
                             <input type="checkbox" id="remember-me">
                             <label for="remember">
@@ -52,33 +42,27 @@
                         </div>
                     </div>
                     <!-- /.col -->
-                    <div class="col-4">
-                        
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    <div class="col-10">
+                            @if (Route::has('password.request'))
+                            <a href="{{ route('password.request') }}">forgot password</a>
+                            </a>
+                            @endif
                     </div>
-                    <!-- /.col -->
+                        
                 </div>
+                        <!-- /.col -->
+                        <p class="mb-1">
+                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+
+                        </p>
             </form>
 
-            <div class="social-auth-links text-center mt-2 mb-3">
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                </a>
-            </div>
+           
             <!-- /.social-auth-links -->
 
-            <p class="mb-1">
-                @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">I forgot my password</a>
-                </a>
-                @endif
-
-            </p>
+           
             <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
+                <a href="register.html" class="text-center">Create account</a>
             </p>
         </div>
         <!-- /.card-body -->
